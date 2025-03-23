a050560812b0a95ea5989ed4d8a72510cceac5eb
Dipec001 <dpecchukwu@gmail.com>
2025-03-23 18:15:15 +0100

    add init

--- my-python-project/tests/test_main.py
+++ my-python-project/tests/test_main.py
@@ -1,5 +1,5 @@
 # test_main.py
-from my-python-project.main import add  # Use the full import path
+from my_python_project.main import add  # Use the full import path
 
 def test_add():
     assert add(2, 3) == 5
